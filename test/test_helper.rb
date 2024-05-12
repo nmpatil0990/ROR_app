5c12064b4da6a2f440a949e7bf8699023c563c5d
Navanit Sonawane <nmpatil0990@gmail.com>
2024-05-12 10:08:01 +0000

    chapter 8

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,14 +4,15 @@ require "rails/test_help"
 
 module ActiveSupport
   
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-    include ApplicationHelper
-
-    # Add more helper methods to be used by all tests here...
-  end
+  class ActiveSupport::TestCase
+     # Run tests in parallel with specified workers
+     parallelize(workers: :number_of_processors)
+     # Set up all fixtures in test/fixtures/*.yml.
+     fixtures :all
+     include ApplicationHelper
+     # Returns true if a test user is logged in.
+     def is_logged_in?
+        !session[:user_id].nil?
+     end
+    end
 end
