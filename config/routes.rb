5990cd9a0c4f2d597795f11ccfaaf45f3efa976f
Navanit Sonawane <nmpatil0990@gmail.com>
2024-05-24 14:46:32 +0000

    password-resete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'users/new'
   root "static_pages#home"  
   get    "/login",   to: "sessions#new"
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   get  "/signup",  to: "users#new"
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
